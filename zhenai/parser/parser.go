c0a6db530e0c0a20dccc9c2860549c8fd1f09137
herzorf <1446450047@qq.com>
2022-12-29 00:11:31 +0800

    抽出获取城市列表的解析器

--- zhenai/parser/parser.go
+++ zhenai/parser/parser.go
@@ -1,1 +1,22 @@
 package parser
+
+import (
+	"github.com/herzorf/gun/engine"
+	"regexp"
+)
+
+const cityListReg = `<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`
+
+func ParseCityList(content []byte) engine.ParserResult {
+	compile := regexp.MustCompile(cityListReg)
+	submatch := compile.FindAllStringSubmatch(string(content), -1)
+	result := engine.ParserResult{}
+	for _, value := range submatch {
+		result.Item = append(result.Item, value[2])
+		result.Requests = append(result.Requests, engine.Request{
+			Url:        value[1],
+			ParserFunc: engine.NilParser,
+		})
+	}
+	return result
+}
