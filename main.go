5a0b80d46b2a6366374a5a728c5170a938db211c
herzorf <1446450047@qq.com>
2022-12-28 15:12:29 +0800

    获取对象网站的网页内容

--- main.go
+++ main.go
@@ -1,8 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
+	response, err := http.Get("https://www.zhenai.com/zhenghun")
 
-	fmt.Println("xxx")
+	if err != nil {
+		panic(err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(response.Body)
+	if response.StatusCode != http.StatusOK {
+		fmt.Println("err: status code ", response.StatusCode)
+		return
+	}
+	all, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(all))
 }
