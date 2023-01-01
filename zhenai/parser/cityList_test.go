9a59e5314cca863e35d3e4daab600147f7f10c89
herzorf <1446450047@qq.com>
2023-01-01 19:44:04 +0800

    添加测试

--- zhenai/parser/cityList_test.go
+++ zhenai/parser/cityList_test.go
@@ -1,14 +1,40 @@
 package parser
 
 import (
-	"github.com/herzorf/gun/fetcher"
+	"fmt"
+	"os"
 	"testing"
 )
 
 func TestParseCityList(t *testing.T) {
-	contents, err := fetcher.Fetcher("http://www.zhenai.com/zhenghun")
+	contents, err := os.ReadFile("cityList_test_data.html")
 	if err != nil {
 		panic(err)
 	}
-	CityList(contents)
+	list := CityList(contents)
+	fmt.Println(len(list.Requests))
+
+	const resultSize = 470
+	expectedUrls := []string{
+		"http://www.zhenai.com/zhenghun/aba", "http://www.zhenai.com/zhenghun/akesu", "http://www.zhenai.com/zhenghun/alashanmeng",
+	}
+	expectedCitys := []string{
+		"阿坝", "阿克苏", "阿拉善盟",
+	}
+	if len(list.Requests) != resultSize {
+		t.Errorf("list should have %d"+"requests; but had %d", resultSize, len(list.Requests))
+	}
+	for i, url := range expectedUrls {
+		if list.Requests[i].Url != url {
+			t.Errorf("expected url #%d: %s; but was %s", i, url, list.Requests[i].Url)
+		}
+	}
+	if len(list.Item) != resultSize {
+		t.Errorf("list should have %d"+"requests; but had %d", resultSize, len(list.Item))
+	}
+	for i, item := range expectedCitys {
+		if list.Item[i] != item {
+			t.Errorf("expected url #%d: %s; but was %s", i, item, list.Item[i])
+		}
+	}
 }
